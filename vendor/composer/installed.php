a7154a7dcda7214ebe35157c7bbd49b6cf8102db
Andreas Gerum <andreas.gerum@savepaper.work>
2020-12-21 23:23:38 +0100

    add composer data

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c248e26ee238ed3e9a0bcce248e00a9ff3334369',
+    'name' => 'andimg93/disposable-mail-detection',
+  ),
+  'versions' => 
+  array (
+    'andimg93/disposable-mail-detection' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c248e26ee238ed3e9a0bcce248e00a9ff3334369',
+    ),
+  ),
+);
